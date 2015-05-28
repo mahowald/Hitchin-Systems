6a63b6383e1eb9e125a258e9589b8cd90b91febb
mahowald <matthew.mahowald@gmail.com>
2015-05-28 00:51:12 -0500

    Updates.
    Folks have been working on Dropbox.

---
+++ auto/4-Hyperkahler-Quotients-Sean.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "4-Hyperkahler-Quotients-Sean"
+ (lambda ()
+    (TeX-add-symbols
+     '("Mbar" 0)
+     '("ad" 0)
+     "bbR"
+     "bbC"
+     "bbZ"
+     "bbA"
+     "bbP"
+     "bbF")))
+
